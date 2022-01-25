3d67def378a90110a49ffe0ed17b4ff275115e8d
nhs219 <heesun_z@naver.com>
2022-01-25 22:49:57 +0900

    chore(삭제) - application.properties .yml 파일로 변경 - BotConfig는 어떻게 사용하는거지?

--- src/main/java/com/example/springBot/BotConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.springBot;
-
-public class BotConfig {
-    public static final String TOKENMYPROJECT = "5043745385:AAFdbOE4W51NVLNnxO5WEdDHEWlRMcrZ0hM";
-    public static final String USERNAMEMYPROJECT = "nohingBot";}
